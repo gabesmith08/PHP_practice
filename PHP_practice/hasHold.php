507afc2e61bdfa6737b87973a7b93c36728ff88a
gsmith7 <gsmith7@iastate.edu>
2019-03-06 19:44:22 -0600

    fully implemented and tested in mySQL

---
+++ PHP_practice/hasHold.php
@@ -0,0 +1,17 @@
+<?php
+
+function hasHold($college)
+{
+    // selects the student's ID, Last Name, College, and Amount they have Due.
+    // only if the student currently an account on hold from unpaid tuition
+	$query = ("SELECT interview_student.Student_ID,
+					  interview_student.Last_Name, 
+					  interview_student.College, 
+					  interview_account_hold.Amount_Due 
+			  FROM interview_student
+			  INNER JOIN interview_account_hold
+			  ON interview_student.Student_ID = interview_account_hold.Student_ID
+			  WHERE interview_student.College = '$college';");
+	return $query;
+}
+?>
